dfa32ae1e9e614b24e2e82d6bf51bdecf3b97840
Pratik Durge <83294285+pldurge@users.noreply.github.com>
2021-07-05 12:54:15 +0530

    Update GlobalExceptionHandler.java

--- CustomerService/src/main/java/com/hallbooking/customer/exception/GlobalExceptionHandler.java
+++ CustomerService/src/main/java/com/hallbooking/customer/exception/GlobalExceptionHandler.java
@@ -11,13 +11,13 @@ public class GlobalExceptionHandler {
 	@ExceptionHandler(IDNotFoundException.class)
 	public ResponseEntity<CustomErrorResponse> exceptionHandler(IDNotFoundException idn)
 	{
-		var resp=new CustomErrorResponse("ID Not Found",idn.getMessage(), HttpStatus.NOT_FOUND.value());
+		CustomErrorResponse resp=new CustomErrorResponse("ID Not Found",idn.getMessage(), HttpStatus.NOT_FOUND.value());
 		return new ResponseEntity<>(resp,HttpStatus.NOT_FOUND);
 	}
 	
 	@ExceptionHandler(CustomException.class)
 	public ResponseEntity<CustomErrorResponse> eHandler(CustomException cesp){
-		var resp=new CustomErrorResponse("Invalid Request",cesp.getMessage(), HttpStatus.BAD_REQUEST.value());
+		CustomErrorResponse resp=new CustomErrorResponse("Invalid Request",cesp.getMessage(), HttpStatus.BAD_REQUEST.value());
 		return new ResponseEntity<>(resp,HttpStatus.BAD_REQUEST);
 	}
 }
